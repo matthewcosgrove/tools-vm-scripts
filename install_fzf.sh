c14d507110ff750e753dd0866fc4b1afa0a4680c
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-09-27 14:39:58 +0100

    Fix fzf install script

--- install_fzf.sh
+++ install_fzf.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dir="~/.fzf"
+dir="/home/ubuntu/.fzf"
 if [[ ! -d "${dir}" ]];then
    # https://github.com/junegunn/fzf#using-git
    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf

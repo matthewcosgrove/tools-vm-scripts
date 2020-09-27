7fd13ac5f7ee1b0a9f33be0cb8c154bd1b04cc36
Matthew Cosgrove <mc.matthew.cosgrove@gmail.com>
2020-09-27 14:24:23 +0100

    Ensure fzf install is idempotent

--- install_fzf.sh
+++ install_fzf.sh
@@ -2,7 +2,11 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# https://github.com/junegunn/fzf#using-git
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+dir="~/.fzf"
+if [[ ! -d "${dir}" ]];then
+   # https://github.com/junegunn/fzf#using-git
+   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+   ~/.fzf/install --key-bindings --completion --update-rc
+else
+   echo "${dir} exists, skipping install of FZF..."
+fi

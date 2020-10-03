a2013b4b8f1d3b1484ddbd59e9dfa51c089c5b4b
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2020-10-03 09:54:29 +0100

    Update docker install script

--- install_docker.sh
+++ install_docker.sh
@@ -3,19 +3,31 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 apt_repo_name="docker"
-if ! grep -q "${apt_repo_name}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+#if ! grep -q "${apt_repo_name}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    sudo apt-get update
+    sudo apt-get -y install \
+                 apt-transport-https \
+                 ca-certificates \
+                 curl \
+                 gnupg-agent \
+                 software-properties-common
     echo "Adding PPA ${apt_repo_name}"
     wget -q -O - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-fi
+#fi
 
-package="docker"
+package="docker-ce"
 if ! hash "${package}" >/dev/null 2>&1; then
     echo "Installing ${package}..."
     sudo apt-get update
     apt-cache policy docker-ce
     sudo apt install "${package}" -y
-    sudo systemctl start docker
+    sudo apt install docker-ce-cli containerd.io -y
+    sudo systemctl unmask docker.service
+    sudo systemctl unmask docker.socket
+    sudo systemctl start docker.service
+    #sudo systemctl is-active --quiet docker && echo Service is running
+    sudo systemctl is-active docker && echo Service is running
     sudo usermod -aG docker ${USER}
     echo "Your current groups are.."
     id -nG
